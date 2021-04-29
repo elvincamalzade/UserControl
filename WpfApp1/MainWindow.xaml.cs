e359c9061914cfd56c2c7a3add957525c2dc66df
Camalzade Elvin <e.camalzade@itstep.edu.az>
2021-04-29 16:45:51 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.Configuration;
+using System.Net;
+using System.Windows;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public ApplicationIdCredentialsProvider Provider { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            // var appId = "PGgXDxXtbqJFb22FYOW4~V9ZWj-mMArtwhcHO7OXOyA~Amvufht9v-dLEV-Fp360l5Jk0Lwhvc8UUO9DNGeO0BMQj3ziwsNsDQADV1M-iewb";
+
+            var appId = ConfigurationManager.AppSettings["BingToken"];
+            var serverId = IPAddress.Parse(ConfigurationManager.AppSettings["ServerIp"]);
+            this.DataContext = this;
+            Provider = new ApplicationIdCredentialsProvider(appId);
+        }
+    }
+}
